0755a33eeb6f25058b0dbd7ca987af68bd820874
Maximos Salzman <maximossalzman@gmail.com>
2020-11-19 16:29:34 -0500

    created sign up form

---
+++ frontend/solo_project_one/src/components/SignUpFormPage/index.js
@@ -0,0 +1,52 @@
+import { set } from 'js-cookie';
+import React, {useState, useEffect} from 'react';
+import * as sessionActions from '../../store/session'
+import {useDispatch, useSelector} from 'react-redux';
+import {Redirect} from 'react-router-dom'
+
+const SignUpFormPage = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user)
+    const state = useSelector(state => state)
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([])
+
+    if(sessionUser) return (
+        <Redirect to="/" />
+    )
+
+    const registerUser = e => {
+        e.preventDefault()
+        setErrors([]);
+        return dispatch(sessionActions.signUp({
+            username, email, password
+        })).catch((res) => {
+            if (res.data && res.data.errors) {
+                setErrors(res.data.errors);
+            }
+        })
+    }
+
+    return (
+        <form onSubmit={registerUser} >
+            <ul>
+                {errors.map((error, idx) => (
+                     <li key={idx}>{error}</li>
+                ))}
+            </ul>
+            <label for="userName">User Name</label>
+            <input type="text" id="userName" onChange={e=>setUsername(e.target.value)}  required value={username} />
+            <label for="email">Email</label>
+            <input type="email" id="email" onChange={e=>setEmail(e.target.value)}  required value={email} />
+            <label for="password">Password</label>
+            <input type="password" id="password" onChange={e=>setPassword(e.target.value)} required value={password} />
+            <button type="submit">Sign Up</button>
+        </form>
+    );
+};
+
+
+export default SignUpFormPage;
