354724a5f5e15a0b1bab3a1bb25590469516a1c3
Maximos Salzman <maximossalzman@gmail.com>
2020-11-19 16:42:05 -0500

    added confirmpassword to signup form

--- frontend/solo_project_one/src/components/SignUpFormPage/index.js
+++ frontend/solo_project_one/src/components/SignUpFormPage/index.js
@@ -1,17 +1,18 @@
-import { set } from 'js-cookie';
 import React, {useState, useEffect} from 'react';
 import * as sessionActions from '../../store/session'
 import {useDispatch, useSelector} from 'react-redux';
 import {Redirect} from 'react-router-dom'
+import './SignUpForm.css'
 
 const SignUpFormPage = () => {
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user)
-    const state = useSelector(state => state)
+    // const state = useSelector(state => state)
 
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     const [errors, setErrors] = useState([])
 
     if(sessionUser) return (
@@ -20,14 +21,17 @@ const SignUpFormPage = () => {
 
     const registerUser = e => {
         e.preventDefault()
-        setErrors([]);
-        return dispatch(sessionActions.signUp({
-            username, email, password
-        })).catch((res) => {
-            if (res.data && res.data.errors) {
-                setErrors(res.data.errors);
-            }
-        })
+        if(password === confirmPassword) {
+            setErrors([]);
+            return dispatch(sessionActions.signUp({
+                username, email, password
+            })).catch((res) => {
+                if (res.data && res.data.errors) {
+                    setErrors(res.data.errors);
+                }
+            })
+        }
+        return setErrors(['Confirm Password and Password must match'])
     }
 
     return (
@@ -43,6 +47,8 @@ const SignUpFormPage = () => {
             <input type="email" id="email" onChange={e=>setEmail(e.target.value)}  required value={email} />
             <label for="password">Password</label>
             <input type="password" id="password" onChange={e=>setPassword(e.target.value)} required value={password} />
+            <label for="confirmPassword">Confirm Password</label>
+            <input type="password" id="confirmPassword" onChange={e=>setConfirmPassword(e.target.value)} required value={confirmPassword} />
             <button type="submit">Sign Up</button>
         </form>
     );
