46b4c66aefead6c64e7b7d74f3527c5e8abe25d2
Maximos Salzman <maximossalzman@gmail.com>
2020-11-30 09:43:34 -0500

    activated home button

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -34,14 +34,14 @@ const Navigation = () => {
         setSearchTerm(e.target.value)
     }
 
-    // const reset = e => {
-    //     return(dispatch(searchActions.setSearchResult()))
-    // }
+    const reset = e => {
+        return(dispatch(searchActions.setSearchResult()))
+    }
 
     return (
         <div className="navbar">
             <div className="logo">
-                <NavLink className="specialButton"to="/" >littleJon</NavLink>
+                <NavLink className="specialButton"to="/" onClick={reset}>littleJon</NavLink>
             </div>
             <form onSubmit={searchStock}>
                 <input className="searchbar" type="search" value={searchTerm}  placeholder="search by stock symbol"  onChange={changeTerm}/>
