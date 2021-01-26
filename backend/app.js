ab4dc69b990acf4324381ef4e2b944c3f58c2ab0
Maximos Salzman <maximossalzman@gmail.com>
2021-01-26 17:38:25 -0500

    fixes typo

--- backend/app.js
+++ backend/app.js
@@ -46,10 +46,10 @@ if (process.env.NODE_ENV === 'production') {
   const path = require('path');
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve('../frontend/build')));
+  app.use(express.static(path.resolve('../frontend/build')));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api  
-  router.get(/^(?!\/?api).*/, (req, res) => {
+  app.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
