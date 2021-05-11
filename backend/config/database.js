c01e0cb9fef88ee3b636991e9dfde81618ca2eed
maximossalzman <maximos.salzman@epicvideo.tech>
2021-05-11 17:47:17 -0400

    adds ssl

--- backend/config/database.js
+++ backend/config/database.js
@@ -19,5 +19,9 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
+    ssl = true,
+    dialectOptions = {
+      ssl: true,
+    },
   },
 };
